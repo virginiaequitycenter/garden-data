eb5579ea4e41f1f0ec2f7b9973809d7db0f4eedf
leboeuf77 <leboeuf77@gmail.com>
2022-04-21 09:58:53 -0400

    updated garden map

--- geocoding-gardens.R
+++ geocoding-gardens.R
@@ -5,6 +5,70 @@ library(sf)
 library(leaflet)
 library(DT)
 
+## This chunk of code makes revisions to the data file "04-12-gardens_geo.RDS"
+# which was created with code below. The revisions are based on feedback from 
+# Richard Morris at Cultivate on 4/14
+
+gardens <- readRDS("04-12-gardens_geo.RDS")
+
+# Fixing the visible records garden to have the correct management 
+gardens$Managed_by[which(gardens$Garden_property_name == "Visible records garden")] <- "Kendall King"
+
+# Fixing the Trinity Episcopal church bread and roses garden to have the correct management
+gardens$Managed_by[which(gardens$Location_description == "Trinity Episcopal Church")] <- "Bread and Roses"
+
+# Editing the garden size information for one garden that Richard Morris had info for
+gardens$Total_size[which(gardens$Garden_property_name == "6th Street")] <- "4400 sq ft"
+gardens$Size[which(gardens$Garden_property_name == "6th Street")] <- "4400 sq ft"
+
+gardens$Total_size[which(gardens$Managed_by == "Urban Agriculture Collective")] <- gardens$Size[which(gardens$Managed_by == "Urban Agriculture Collective")]
+
+# The 6th street garden was lost, but it has been revived
+gardens$Status[which(gardens$Garden_property_name == "6th Street")] <- "Existing"
+
+## Manually fixing the geolocations 
+# West St garden = 38.036166511857765, -78.48929141009091
+gardens$geometry[which(gardens$Garden_property_name == "West St")] <- st_point(c(-78.48929141009091, 38.036166511857765))
+
+# CATEC garden = 38.06224403490436, -78.46482075941262
+gardens$geometry[which(gardens$Garden_property_name == "CATEC School")] <- st_point(c(-78.46482075941262, 38.06224403490436))
+
+# South 1st St = 38.019160547205594, -78.48690403419546
+gardens$geometry[which(gardens$Garden_property_name == "South 1st St")] <- st_point(c(-78.48690403419546, 38.019160547205594))
+
+# 6th st = 38.02520028910517, -78.48115967430914
+gardens$geometry[which(gardens$Garden_property_name == "6th Street")] <- st_point(c(-78.48115967430914, 38.02520028910517))
+
+# Friendship court = 38.025798361893564, -78.48067282927684
+gardens$geometry[which(gardens$Garden_property_name == "Friendship Court")] <- st_point(c(-78.48067282927684, 38.025798361893564))
+
+# Azalea Park gardens = 38.01023749045567, -78.5138139458892
+gardens$geometry[which(gardens$Garden_property_name == "Azalea Park Garden")] <- st_point(c(-78.5138139458892, 38.01023749045567))
+
+# Meadow creek garden = 38.05673992416423, -78.49333775468119
+gardens$geometry[which(gardens$Garden_property_name == "Meadow Creek Garden")] <- st_point(c(-78.49333775468119, 38.05673992416423))
+
+# Michie Drive = 38.05948411617561, -78.48700270170862
+gardens$geometry[which(gardens$Garden_property_name == "Michie Drive")] <- st_point(c(-78.48700270170862, 38.05948411617561))
+
+# Adding additional category column based on whether the garden is managed by a group or specific person
+gardens$cat <- ifelse(gardens$Managed_by == "IRC New Roots" |
+                        gardens$Managed_by == "Urban Agriculture Collective" |
+                        gardens$Managed_by == "Charlottesville Parks Department" |
+                        gardens$Managed_by == "City Schoolyard Garden",
+                      gardens$Managed_by, "Other")
+
+# Saving data with updated geolocations 
+write.csv(gardens, '04-21-gardens_geo.csv', row.names = F)
+write_rds(gardens, "04-21-gardens_geo.RDS")
+
+
+## Initial work to geo-code gardens. This chunk of code created the data file 
+# called "04-12-gardens_geo.csv". I used open street maps and approximate addresses
+# for some of the gardens to retrieve their geolocations. 
+# We received feedback from Richard Morris at Cultivate on 4/14 that some of the geolocations
+# for specific gardens were not precise, so in the above code, I manually edited them. 
+
 gardens <- read.csv("gardens_nogeo.csv")
 
 ## Need to remove the columns that don't have the address--we have their geo-location from the data that Taha shared
@@ -19,7 +83,6 @@ locations_df <- geocode_OSM(
   geometry = "point",
   server = "https://nominatim.openstreetmap.org"
 )
-
 
 # Manually adding lat and long for one garden that didn't work with 
 # open street map (found the lat and lon on google maps)
